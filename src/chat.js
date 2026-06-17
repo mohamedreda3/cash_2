fd257a2eca8e2e6b62438a278037a293b981c677
mohamedreda3 <mmoh33653@gmail.com>
2026-06-17 10:11:42 +0300

    Chat

--- src/chat.js
+++ src/chat.js
@@ -1,9 +1,10 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { onValue, push, ref, remove, update, get } from "firebase/database";
 import { useInView } from "react-intersection-observer";
 import "./adminPanel.css";
 import { database } from "./firebase";
 import moment from "moment/moment";
+import { toast } from "react-toastify";
 
 const AdminPanel = ({ isHeader, setIfUnRead, setUnReadCountChats }) => {
   const [chats, setChats] = useState([]);
@@ -44,6 +45,8 @@ const AdminPanel = ({ isHeader, setIfUnRead, setUnReadCountChats }) => {
   }, [unreadCount]);
 
   // Effect to fetch messages for the selected chat
+  const messagesEndRef = useRef();
+
   useEffect(() => {
     if (selectedChatId) {
       const chatMessagesRef = ref(database, `chats/${selectedChatId}/messages`);
@@ -59,7 +62,9 @@ const AdminPanel = ({ isHeader, setIfUnRead, setUnReadCountChats }) => {
 
         setMessages(newMessages);
       });
-
+      if (messagesEndRef.current) {
+        messagesEndRef.current.scrollIntoView({ behavior: "smooth" });
+      }
       return () => unsubscribe();
     }
   }, [selectedChatId]);
@@ -91,21 +96,35 @@ const AdminPanel = ({ isHeader, setIfUnRead, setUnReadCountChats }) => {
               unreadMessages++;
             }
           });
-
           // Check for new messages and play sound if there are any
           const lastTimestamp = lastMessageTimestamps[chatId] || 0;
           const latestMessage = messages[messages.length - 1];
+
+          const latestMessageDate = moment(
+            latestMessage.createdAt,
+            "MMMM Do YYYY, h:mm:ss a"
+          );
+          const currentDate = moment();
+          const modifiedDate = currentDate.clone().add(2, "seconds");
+
+          console.log(
+            latestMessageDate.format("MMMM Do YYYY, h:mm:ss a"),
+            modifiedDate.format("MMMM Do YYYY, h:mm:ss a"),
+            latestMessageDate.isSameOrAfter(modifiedDate)
+          );
+
           if (
             latestMessage &&
-            new Date(latestMessage.createdAt).getTime() > lastTimestamp &&
+            latestMessageDate.isSameOrAfter(modifiedDate) &&
             latestMessage?.from !== "Admin"
           ) {
             if (!initialLoad) {
-              playNotificationSound();
+            // toast.dark("رسالة جديدة")
+            playNotificationSound();
             }
             setLastMessageTimestamps((prev) => ({
               ...prev,
-              [chatId]: new Date(latestMessage.createdAt).getTime(),
+              [chatId]: moment(latestMessage.createdAt).valueOf(),
             }));
           }
 
@@ -121,25 +140,34 @@ const AdminPanel = ({ isHeader, setIfUnRead, setUnReadCountChats }) => {
     return () => unsubscribe();
   }, [lastMessageTimestamps, initialLoad]);
   useEffect(() => {
-    if (unreadCount) {
-      try {
-        if (setIfUnRead)
-          setIfUnRead(
-            Object.keys(unreadCount)
-              ?.map((item) => item)
-              ?.reduce((acc, cur) => unreadCount[cur] + acc, 0)
-          );
-        if (setUnReadCountChats)
-          setUnReadCountChats(
-            Object.keys(unreadCount)
-              ?.map((item) => item)
-              ?.filter((item) => unreadCount[item] > 0)?.length
-          );
-      } catch (e) {
-        console.error(e);
-      }
-    }
+    const chatRef = ref(database, `chats`);
+
+    const unsubscribe = onValue(chatRef, (snapshot) => {
+      const data = snapshot.val();
+      const newData = data
+        ? Object.keys(data)
+            .map((key) => {
+              const chat = {
+                id: key,
+                name: null,
+                unread: unreadCount[key] || 0,
+              };
+              if (data[key]?.messages) {
+                const firstMessage = Object.values(data[key].messages)[0];
+                chat.name = firstMessage.displayName;
+              }
+              return chat;
+            })
+            .filter((chat) => chat.name !== null)
+            .sort((a, b) => b.unread - a.unread) // Sort by unread messages (descending)
+        : [];
+
+      setChats(newData);
+    });
+
+    return () => unsubscribe();
   }, [unreadCount]);
+
   // Function to play notification sound
   const playNotificationSound = () => {
     const audioElement = document.getElementById("messageSound");
@@ -216,14 +244,14 @@ const AdminPanel = ({ isHeader, setIfUnRead, setUnReadCountChats }) => {
         );
         await update(messageRef, {
           text: newMessage,
-          editedAt: moment().format('MMMM Do YYYY, h:mm a'),
+          editedAt: moment().format("MMMM Do YYYY, h:mm:ss a"),
         });
         setEditMessageId(null);
       } else {
         const messagesRef = ref(database, `chats/${selectedChatId}/messages`);
         await push(messagesRef, {
           text: newMessage,
-          createdAt: moment().format('MMMM Do YYYY, h:mm a'),
+          createdAt: moment().format("MMMM Do YYYY, h:mm:ss a"),
           from: "Admin",
           displayName: "Admin",
           read: false,
@@ -231,7 +259,6 @@ const AdminPanel = ({ isHeader, setIfUnRead, setUnReadCountChats }) => {
         });
       }
       setNewMessage("");
-    
     } catch (error) {
       console.error("Error sending message:", error);
     }
@@ -257,6 +284,31 @@ const AdminPanel = ({ isHeader, setIfUnRead, setUnReadCountChats }) => {
     setEditMessageId(msg.id);
   };
 
+  const handleEndChat = async () => {
+    if (!selectedChatId) return; // Exit early if no chat is selected
+
+    if (
+      !window.confirm(
+        "Are you sure you want to delete all messages in this chat?"
+      )
+    ) {
+      return;
+    }
+
+    try {
+      const chatMessagesRef = ref(database, `chats/${selectedChatId}/messages`);
+      await remove(chatMessagesRef);
+      setMessages([]);
+    } catch (error) {
+      console.error("Error ending chat:", error);
+    }
+  };
+  const handleKeyPress = (event) => {
+    if (event.key === "Enter") {
+      event.preventDefault(); // Prevents the default action of the Enter key (e.g., form submission)
+      handleSend(); // Calls the function to send the message
+    }
+  };
   // Function to cancel editing a message
   const handleCancelEdit = () => {
     setNewMessage("");
@@ -306,6 +358,7 @@ const AdminPanel = ({ isHeader, setIfUnRead, setUnReadCountChats }) => {
             onChange={(e) => setNewMessage(e.target.value)}
             placeholder="Type your message..."
             className="chat-input"
+            onKeyPress={handleKeyPress}
           />
           <button onClick={handleSend} className="chat-send-button">
             {editMessageId ? (
@@ -319,6 +372,9 @@ const AdminPanel = ({ isHeader, setIfUnRead, setUnReadCountChats }) => {
               ✕
             </button>
           )}
+          <button className="btn btn-danger" onClick={() => handleEndChat()}>
+            إنهاء الدردشة
+          </button>
         </div>
       </div>
     </div>
